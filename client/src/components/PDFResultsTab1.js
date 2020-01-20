d9d767c1355a1a84b9c8b65a672d5df18dec40c9
jonasbarre <55202983+jonasbarre@users.noreply.github.com>
2020-01-20 17:28:14 +0100

    Update PDFResultsTab1.js

--- client/src/components/PDFResultsTab1.js
+++ client/src/components/PDFResultsTab1.js
@@ -1418,7 +1418,7 @@ class ResultsTab1 extends React.Component {
                         <div className="form-row-header scenario-header">
                           <h4 className="heading">Samenwerking Scenario 2</h4>
                           <p>
-                            De school werkt structureel samen met externe
+                            De school werkt regelmatig samen met externe
                             partners en – deskundigen. De inhoudelijke keuzes
                             liggen bij de externe partner (afnemen
                             cultuuraanbod).
